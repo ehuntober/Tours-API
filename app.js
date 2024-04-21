6087714814839d69d324b367cf5e11cbbb44712c
ehuntober <ehuntercrypto@gmail.com>
2024-04-21 13:03:31 +0100

    new file added

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.get('/api/v1/tours', (req,res) =>{
 app.post('/api/v1/tours',(req,res) =>{
     const newId = tours[tours.length - 1].id + 1;
     const newTour = Object.assign({id: newId},req.body)
-    
+
     tours.push(newTour)
 
     fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours), err=>{
@@ -43,7 +43,7 @@ app.post('/api/v1/tours',(req,res) =>{
 
     })
 
-    res.send('Done');
+   
     
 
 })
