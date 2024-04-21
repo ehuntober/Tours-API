6e5ce8a8649788792f222e13b0953de3739c1c28
ehuntober <ehuntercrypto@gmail.com>
2024-04-21 13:49:35 +0100

    app.js error modified

--- app.js
+++ app.js
@@ -46,6 +46,13 @@ app.post('/api/v1/tours',(req,res) =>{
 
 app.get('/api/v1/tours/:id', (req,res) =>{
     const id = req.params.id * 1;
+
+    if(id > tour.length){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
     const tour = tours.find(el=> el.id === id);
     res.status(200).json({
         status: 'sucess',
