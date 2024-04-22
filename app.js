b28cfd3668893d3fd7917fc7e0d7ea9fb1f90dfe
ehuntober <ehuntercrypto@gmail.com>
2024-04-22 13:48:11 +0100

    added up

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
 const fs = require('fs')
 const app = express();
+
+
+app.use(morgan('dev'))
 app.use(express.json())
+
 
 app.use((req,res,next)=>{
     console.log('Hello from the middleware')
