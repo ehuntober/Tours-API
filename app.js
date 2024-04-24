0f5c0d48b0172b83d82c6ae0598150cfcff01fcc
ehuntober <ehuntercrypto@gmail.com>
2024-04-24 10:47:30 +0100

    new file added

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
+// app.js
+
 const express = require('express');
 const morgan = require('morgan');
 const fs = require('fs');
-
-// Correct the import paths for tourRouter and userRouter
-const tourRouter = require('./routes/tourRoutes'); // Add require()
-const userRouter = require('./routes/userRoutes'); // Add require()
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 
 // ROUTES
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 
-
-//    const port = 3000;
-//    app.listen(port, () =>{
-//     console.log(`App running on port ${port}`)
-// });
+module.exports = app; // Export the app object
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-const app = require('./app');
-const port = 3000;
+const app = require('./app'); // Import the app object from the app.js file
 
-console.log(app.get('env'))
-app.listen(port, () =>{
-    console.log(`App running on port ${port}...`)
-})
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
