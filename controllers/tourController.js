1c6ca1ab6c24684ffb366ddc46fad08afaa65bd9
ehuntober <ehuntercrypto@gmail.com>
2024-04-25 07:26:08 +0100

    new file updated

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -146,7 +146,7 @@ exports.updateTour = async(req,res)=>{
     }
 }
 
-exports.deleteTour = (req,res)=>{
+exports.deleteTour = async (req,res)=>{
     // if(req.params.id * 1 > tours.length){
     //     return res.status(404).json({
     //         status: 'fail',
@@ -155,7 +155,7 @@ exports.deleteTour = (req,res)=>{
     // }
 
     try{
-        Tour.findByIdAndDelete(req.params.id);
+      await  Tour.findByIdAndDelete(req.params.id);
         res.status(204).json({
             status: "success",
             data:null
