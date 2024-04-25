cd53abe285a2e739dba46415df8f138bfdf9fcdd
ehuntober <ehuntercrypto@gmail.com>
2024-04-25 03:17:02 +0100

    tour controller created

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -86,6 +86,11 @@ exports.createTour = async(req,res) =>{
         })
 
     }catch(err){
+        res.status(400).json({
+            status: 'fail',
+            message: err
+        })
+
 
     }
 }
