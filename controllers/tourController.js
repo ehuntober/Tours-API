f4540de1f4c66a0586d0c7d77214cdc97a04799b
ehuntober <ehuntercrypto@gmail.com>
2024-04-25 14:18:31 +0100

    fixed the sorting error

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -28,11 +28,47 @@ exports.checkBody = (req,res,next) =>{
 
 exports.getAllTours = async (req, res) => {
   try {
-    const queryObj = {...req.query};
-    const excludedFields = ['page','sort', 'limit', 'fields'];
-    excludedFields.forEach(el=> delete queryObj[el]);
-    
-    const tours = await Tour.find(req.query);
+    // BUILD QUERY
+    // 1A) Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    // 1B) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    if(req.query.sort){
+      const sortBy = req.query.sort.split(',').join('');
+      query = query.sort(sortBy)
+    } else{
+      query = query.sort('-createdAt')
+    }
+
+    // 3) Field limiting
+    if(req.query.fields){
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else{
+      query = query.select('-__v')
+    }
+
+    // 4) pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit *1 || 100
+    const skip = (page -1) * limit
+
+    // page=3&limit=10, 1-10, page 1, 11-20, page 2, 21-30, page3
+
+    query = query.skip(skip).limit(limit)
+
+
+    // EXECUTE QUERY
+    const tours = await query.exec();
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
@@ -45,7 +81,7 @@ exports.getAllTours = async (req, res) => {
       status: 'error',
       message: 'Something went wrong',
     });
-    console.log(err)
+    console.log(err);
   }
 };
 
