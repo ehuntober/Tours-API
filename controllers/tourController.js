a896018370280bb5a70eb476c4ceb0a0abfab84f
ehuntober <ehuntercrypto@gmail.com>
2024-04-26 04:27:55 +0100

    new file added

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -64,9 +64,15 @@ exports.getAllTours = async (req, res) => {
 
     query = query.skip(skip).limit(limit)
 
+    if(req.query.page){
+      const numTours = await Tour.countDocuments();
+      if(skip >= numTours) throw new Error('This page does not exist')
+    }
+
 
     // EXECUTE QUERY
     const tours = await query.exec();
+    // query.sort().select().skip().limit()
 
     // SEND RESPONSE
     res.status(200).json({
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 // router.param('id', tourController.checkID)
 
-
+router.route('/top-5-cheap').get(tourController.getAllTours)
 router
 .route('/')
 .get(tourController.getAllTours)
