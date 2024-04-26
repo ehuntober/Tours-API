9152123f0c62fec2672e46ec883ffd3ebd50babc
ehuntober <ehuntercrypto@gmail.com>
2024-04-26 15:18:40 +0100

    installed slugify

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const slugify = require('slugify')
 const tourSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -6,6 +7,7 @@ const tourSchema = new mongoose.Schema({
         unique: true,
         trim: true
     },
+    slug: String,
     duration:{
         type:Number,
         required:[true,'A tour must have a duration']
@@ -51,7 +53,11 @@ const tourSchema = new mongoose.Schema({
         default: Date.now(),
         select: false
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour:{
+        type:Boolean,
+        default: false
+    }
 },
 {
     toJSON: { virtuals: true},
@@ -59,18 +65,37 @@ const tourSchema = new mongoose.Schema({
 })
 
 
+
+tourSchema.virtual('durationWeeks').get(function( next){
+    return this.duration / 7;
+    
+    // next()
+})
+
+
 // DOCUMENT MIDDLEWARE: rns before .save( ) and . create()
 tourSchema.pre('save', function(){
-    console.log(this)
+    this.slug = slugify(this.name, {lower: true})
 
 })
 
-tourSchema.virtual('durationWeeks').get(function(){
-    return this.duration / 7;
+// tourSchema.pre('save', function(next){
+//     console.log('will save document...')
+// })
+
+// tourSchema.post('save', function(doc, next){
+//     console.log(doc);
+//     next();
+
+// })
 
 
+
+// QUERY MIDDLEWARE
+tourSchema.pre('find', function(next){
+
+    next()
 })
-
 const  Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
