4db9f8718ec4e71dbffb7c9250409d89e52a0987
ehuntober <ehuntercrypto@gmail.com>
2024-04-26 09:35:41 +0100

    added new function to the class API features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -73,6 +73,24 @@ class APIFeatures{
     }
     return this;
   }
+
+  paginate(){
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit *1 || 100
+    const skip = (page -1) * limit
+
+    // page=3&limit=10, 1-10, page 1, 11-20, page 2, 21-30, page3
+
+    this.query = this.query.skip(skip).limit(limit)
+
+    if(this.queryString.page){
+      const numTours = await Tour.countDocuments();
+      if(skip >= numTours) throw new Error('This page does not exist')
+    }
+      return this;
+  }
+
+
 }
 
 exports.getAllTours = async (req, res) => {
