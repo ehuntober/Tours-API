9676da67a7f26e2abd667de01b20815ac04d693e
ehuntober <ehuntercrypto@gmail.com>
2024-04-27 11:19:07 +0100

    integerated error handler function to the code

--- app.js
+++ app.js
@@ -1,45 +1,36 @@
 // app.js
-
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError')
+const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-if(process.env.NODE_ENV === "development"){
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan('dev'));
 }
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-
-app.use((req,res,next) =>{
-    console.log('Hello from the middleware')
-    next();
-})
-
-app.all('*', (req,res,next) =>{
-    res.status(404).json({
-        status: 'fail',
-        message: `Can't find ${req.originalUrl}`
-    })
-})
-
-app.use((err,req,res,next) =>{
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error'
-
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    })
-})
-
-
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
+});
 
 // ROUTES
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
+
+app.all('*', (req, res, next) => {
+//   const err = new Error(`Can't find ${req.originalUrl}`);
+//   err.status = 'fail';
+//   err.statusCode = 404;
+  next(new AppError(`Can't find ${req.originalUrl}`));
+});
+
+app.use(globalErrorHandler);
+
 
 module.exports = app; // Export the app object
