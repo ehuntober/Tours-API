54371411e9d1c544b6805c59017adda8c53376ab
ehuntober <ehuntercrypto@gmail.com>
2024-04-27 09:38:10 +0100

    modified the app.js file

--- app.js
+++ app.js
@@ -11,11 +11,16 @@ const app = express();
 if(process.env.NODE_ENV === "development"){
     app.use(morgan('dev'));
 }
-console.log(process.env.NODE_ENV)
 
-app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
+
+app.use((req,res,next) =>{
+    console.log('Hello from the middleware')
+    next();
+})
+
+
 
 // ROUTES
 app.use('/api/v1/users', userRouter);
