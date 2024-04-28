9a877d1a3af5073ac28dd85888499fe7b2529c57
ehuntober <ehuntercrypto@gmail.com>
2024-04-28 10:07:13 +0100

    built a new route for the build

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const userController = require('../controllers/userController')
+const authController = require('../controllers/authController')
 
+
+router.post('/signup', authController.signup)
 
 router.route('/')
    .get(userController.getAllUsers)
