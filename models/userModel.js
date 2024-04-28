a0a49351b7cc48ce99f312a5d44eb04ecd919f2c
ehuntober <ehuntercrypto@gmail.com>
2024-04-28 12:13:51 +0100

    added json webtoken for signing new user

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 
 const mongoose = require('mongoose');
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -33,6 +34,19 @@ const userSchema = new mongoose.Schema({
         }
 
     }    
+})
+
+
+userSchema.pre('save',async function(next) {
+// Only run this function if password was actually modified
+    if(!this.isModified('password')) return next();
+
+// Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+
+// Delete passwordConfirm field
+    this.passwordConfirm = undefined;
+    next();
 })
 
 const User = mongoose.model('User',userSchema)
