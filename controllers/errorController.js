408f593e4975b91f0728dca1f5c22084cd8967a1
ehuntober <ehuntercrypto@gmail.com>
2024-04-30 11:40:24 +0100

    fixed error in the code

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,5 +1,83 @@
-const AppError = require('./../utils/appError');
+// const AppError = require('./../utils/appError');
 
+// require('dotenv').config();
+
+// const handleCastErrorDB = err => {
+//   const message = `Invalid ${err.path}: ${err.value}.`;
+//   return new AppError(message, 400);
+// };
+
+// const handleDuplicateFieldsDB = err => {
+//   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+//   const message = `Duplicate field value: ${value}. Please use another value!`;
+//   return new AppError(message, 400);
+// };
+
+// const handleValidationErrorDB = err => {
+//   const errors = Object.values(err.errors).map(el => el.message);
+//   const message = `Invalid input data. ${errors.join('. ')}`;
+//   return new AppError(message, 400);
+// };
+
+
+// const handleJWTError = err => new AppError('Invalid token, Please log in again!', 401)
+
+
+// const handleJWTExpiredError = err => new AppError('Your token has expired! please log in again')
+// const sendErrorDev = (err, res) => {
+//   res.status(err.statusCode).json({
+//     status: err.status,
+//     error: err,
+//     message: err.message,
+//     stack: err.stack
+//   });
+// };
+
+// const sendErrorProd = (err, res) => {
+//   // Operational, trusted error: send message to client
+//   if (err.isOperational) {
+//     res.status(err.statusCode).json({
+//       status: err.status,
+//       message: err.message
+//     });
+//   // Programming or other unknown error: don't leak error details
+//   } else {
+//     // 1) Log error
+//     console.error('ERROR 💥', err);
+//     // 2) Send generic message
+//     res.status(500).json({
+//       status: 'error',
+//       message: 'Something went very wrong!'
+//     });
+//   }
+// };
+
+// module.exports = (err, req, res, next) => {
+//   // console.log(err.stack);
+//   err.statusCode = err.statusCode || 500;
+//   err.status = err.status || 'error';
+//   // console.log(err, err.statusCode )
+
+//   if (process.env.NODE_ENV === 'development') {
+//     sendErrorDev(err, res);
+//   } else if (process.env.NODE_ENV === 'production') {
+//     let error = { ...err };
+
+//     if (error.name === 'CastError') error = handleCastErrorDB(error);
+//     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+//     if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
+//     if (error.name === 'JsonWebTokenError') error = handleJWTError(error);
+//     if (error.name === 'TokenExpiredError') error = handleJWTExpiredError(error)
+
+//     sendErrorProd(error, res);
+//   }
+// };
+
+
+
+
+
+const AppError = require('./../utils/appError');
 require('dotenv').config();
 
 const handleCastErrorDB = err => {
@@ -19,11 +97,9 @@ const handleValidationErrorDB = err => {
   return new AppError(message, 400);
 };
 
+const handleJWTError = err => new AppError('Invalid token, Please log in again!', 401);
+const handleJWTExpiredError = err => new AppError('Your token has expired! please log in again', 401);
 
-const handleJWTError = err => new AppError('Invalid token, Please log in again!', 401)
-
-
-const handleJWTExpiredError = err => new AppError('Your token has expired! please log in again')
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -56,7 +132,7 @@ module.exports = (err, req, res, next) => {
   // console.log(err.stack);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  console.log(err, err.statusCode )
+  // console.log(err, err.statusCode )
 
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
@@ -67,7 +143,7 @@ module.exports = (err, req, res, next) => {
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
     if (error.name === 'JsonWebTokenError') error = handleJWTError(error);
-    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError(error)
+    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError(error);
 
     sendErrorProd(error, res);
   }
