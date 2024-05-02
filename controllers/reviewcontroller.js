1133839dd977d80f96581f335ebc7fff617fb5d7
ehuntober <ehuntercrypto@gmail.com>
2024-05-02 10:12:56 +0100

    new file updated

---
+++ controllers/reviewcontroller.js
@@ -0,0 +1,30 @@
+const Review = require('./../model/reviewModel');
+const catchAsync = require('./../utils/catchAsync')
+
+
+
+
+exports.getAllReviews = async (req,res,next) =>{
+
+    const reviews = await Review.find();
+
+    res.status(200).json({
+        status: 'success',
+        results: reviews.length,
+        data:{
+            reviews
+        }
+    })
+
+}
+
+exports.createReview = catchAsync(async(req,res,enxt)=>{
+    const newReview = await Review.create(req.body);
+
+    res.status(201).json({
+        status: 'success',
+        data:{
+            review: newReview
+        }
+    })
+})
