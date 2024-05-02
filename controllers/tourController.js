3ad54877b7e3b6597db39e227894be7aa896f0f2
ehuntober <ehuntercrypto@gmail.com>
2024-05-02 12:08:12 +0100

    parent data populating

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -37,7 +37,7 @@ exports.getTour = catchAsync(async (req, res, next) => {
     // })
   // Tour.findOne({ _id: req.params.id })
 
-  const tour = await Tour.findById(req.params.id)
+  const tour = await Tour.findById(req.params.id).populate('reviews')
 
   if (!tour) {
     return next(new AppError('No tour found with that ID', 404));
