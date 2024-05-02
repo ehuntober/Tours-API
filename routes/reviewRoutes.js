08a8c0993576759ebf1f23e1dcefe4740dc4168c
ehuntober <ehuntercrypto@gmail.com>
2024-05-02 11:10:42 +0100

    added review route

---
+++ routes/reviewRoutes.js
@@ -0,0 +1,13 @@
+
+const express = require('express')
+const reviewController =  require('./../controllers/reviewcontroller')
+
+const router = express.Router()
+
+
+
+router.route('/')
+
+
+module.exports = router;
+
